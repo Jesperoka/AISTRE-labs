27c9e2fe4f3f627dc8df1e9559ea411638d37d8a
Jesper Ekstrøm <jesperek@stud.ntnu.no>
2023-03-02 14:25:47 +0100

    testing path constraints and other  basic functionality

--- src/main/java/nl/tudelft/instrumentation/symbolic/SymbolicExecutionLab.java
+++ src/main/java/nl/tudelft/instrumentation/symbolic/SymbolicExecutionLab.java
@@ -38,48 +38,124 @@ public class SymbolicExecutionLab {
 
     static MyVar createInput(String name, Expr value, Sort s){
         // Create an input var, these should be free variables!
-        return new MyVar(PathTracker.ctx.mkString(""));
+        Context c = PathTracker.ctx;
+        Expr z3var = c.mkConst(c.mkSymbol(name + "_" + PathTracker.z3counter++), s);
+        PathTracker.addToModel(c.mkEq(z3var, value)); // don't add to model since they're free?
+        return new MyVar(z3var, name);
     }
 
     static MyVar createBoolExpr(BoolExpr var, String operator){
         // Any unary expression (!)
-        return new MyVar(PathTracker.ctx.mkFalse());
+        Context c = PathTracker.ctx;
+        switch (operator) {
+            case "!":
+                PathTracker.addToModel(c.mkNot(var));
+                return new MyVar(c.mkNot(var));
+            case "":
+                PathTracker.addToModel(var);
+                return new MyVar(var);
+            default:
+                throw new IllegalArgumentException("\nUnexpected operator: "+ operator);
+        }
     }
 
     static MyVar createBoolExpr(BoolExpr left_var, BoolExpr right_var, String operator){
         // Any binary expression (&, &&, |, ||)
-        return new MyVar(PathTracker.ctx.mkFalse());
+        Context c = PathTracker.ctx;
+        switch (operator) {
+            case "&":
+            case "&&":
+                PathTracker.addToModel(c.mkAnd(left_var, right_var));
+                return new MyVar(c.mkAnd(left_var, right_var));
+            case "|":
+            case "||":
+                PathTracker.addToModel(c.mkOr(left_var, right_var));
+                return new MyVar(c.mkOr(left_var, right_var));
+            default:
+                throw new IllegalArgumentException("\nUnexpected operator: "+ operator);
+        }
     }
 
     static MyVar createIntExpr(IntExpr var, String operator){
         // Any unary expression (+, -)
-        if(operator.equals("+") || operator.equals("-"))
-            return new MyVar(PathTracker.ctx.mkInt(0));
-        return new MyVar(PathTracker.ctx.mkFalse());
+        Context c = PathTracker.ctx;
+        switch (operator) {
+            case "+":
+                return new MyVar(var);
+            case "-":
+                return new MyVar(c.mkUnaryMinus(var));
+            default:
+                throw new IllegalArgumentException("\nUnexpected operator: "+ operator);
+        }
     }
 
     static MyVar createIntExpr(IntExpr left_var, IntExpr right_var, String operator){
-        // Any binary expression (+, -, /, etc)
-        if(operator.equals("+") || operator.equals("-") || operator.equals("/") || operator.equals("*") || operator.equals("%") || operator.equals("^"))
-            return new MyVar(PathTracker.ctx.mkInt(0));
-        return new MyVar(PathTracker.ctx.mkFalse());
+        // Any binary expression (+, -, /, *, %, ^, etc)
+        Context c = PathTracker.ctx;
+        switch (operator) {
+            // Arithmetic expressions (can't be added)
+            case "+":
+                return new MyVar(c.mkAdd(left_var, right_var));
+            case "-":
+                return new MyVar(c.mkSub(left_var, right_var));
+            case "/":
+                return new MyVar(c.mkDiv(left_var, right_var));
+            case "*":
+                return new MyVar(c.mkMul(left_var, right_var));
+            case "%":
+                return new MyVar(c.mkMod(left_var, right_var));
+            case "^":
+                return new MyVar(c.mkPower(left_var, right_var));
+            // Boolean expressions (can be added)
+            case "<":
+                PathTracker.addToModel(c.mkGt(left_var, right_var));
+                return new MyVar(c.mkGt(left_var, right_var));
+            case ">":
+                PathTracker.addToModel(c.mkLt(left_var, right_var));
+                return new MyVar(c.mkLt(left_var, right_var));
+            case "<=":
+                PathTracker.addToModel(c.mkLe(left_var, right_var));
+                return new MyVar(c.mkLe(left_var, right_var));
+            case ">=":
+                PathTracker.addToModel(c.mkGe(left_var, right_var));
+                return new MyVar(c.mkGe(left_var, right_var));
+            default:
+                throw new IllegalArgumentException("\nUnexpected operator: "+ operator);
+        }
     }
 
     static MyVar createStringExpr(SeqExpr left_var, SeqExpr right_var, String operator){
         // We only support String.equals
-        return new MyVar(PathTracker.ctx.mkFalse());
+        Context c = PathTracker.ctx;
+        System.out.println("createStringExpr() operator: " + operator);
+        switch (operator) {
+            case "=":
+            case "==":
+            case "equals":
+                PathTracker.addToModel(c.mkEq(left_var, right_var));
+                return new MyVar(c.mkEq(left_var, right_var));
+            default:
+                throw new IllegalArgumentException("\nUnexpected operator: "+ operator);
+        }
     }
 
     static void assign(MyVar var, String name, Expr value, Sort s){
         // All variable assignments, use single static assignment
+        Context c = PathTracker.ctx;
+        Expr z3var = c.mkEq(c.mkConst(c.mkSymbol(name + "_" + PathTracker.z3counter++), s), value);
+        PathTracker.addToModel(c.mkEq(z3var, value));
     }
 
     static void encounteredNewBranch(MyVar condition, boolean value, int line_nr){
         // Call the solver
+        Status staus = PathTracker.solver.check();
+        System.out.println("Solver says: " + staus);
+        System.out.println("Model: \n" + PathTracker.solver.getModel());
     }
 
     static void newSatisfiableInput(LinkedList<String> new_inputs) {
         // Hurray! found a new branch using these new inputs!
+        System.out.println("newSatisfiableInput(new_inputs), new_inputs: " + new_inputs);
     }
 
     /**
@@ -117,6 +193,8 @@ public class SymbolicExecutionLab {
         // Place here your code to guide your fuzzer with its search using Symbolic Execution.
         while(!isFinished) {
             // Do things!
+            currentTrace = fuzz(PathTracker.inputSymbols);
+            PathTracker.runNextFuzzedSequence(currentTrace.toArray(new String[0]));
             try {
                 System.out.println("Woohoo, looping!");
                 Thread.sleep(1000);
