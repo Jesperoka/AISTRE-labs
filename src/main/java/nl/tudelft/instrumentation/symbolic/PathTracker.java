b9dec94f6fe117fb175a1665baee815453517d56
Jesper Ekstrøm <jesperoka@gmail.com>
2023-03-14 00:27:43 +0100

    solver returns satisfiability

--- src/main/java/nl/tudelft/instrumentation/symbolic/PathTracker.java
+++ src/main/java/nl/tudelft/instrumentation/symbolic/PathTracker.java
@@ -25,7 +25,7 @@ public class PathTracker {
     static CallableTraceRunner<Void> problem;
     static String[] inputSymbols;
     // Longest a single testcase is allowed to run
-    static final int timeoutMS = 10000;
+    static final int timeoutMS = 60*1000;
     
     /**
      * Used to reset the constraints and everything else of z3 before running the next sequence.
@@ -58,7 +58,7 @@ public class PathTracker {
      * @param printModel boolean value that specifies whether the path constraint should
      *                   be printed in the terminal or not.
      */
-    public static void solve(BoolExpr new_branch, boolean printModel){
+    public static Status solve(BoolExpr new_branch, boolean printModel){
         // Save the state of the solver before adding the branch constraint
         solver.push();
         solver.add(new_branch);
@@ -72,19 +72,20 @@ public class PathTracker {
             System.out.println(new_branch);
         }
 
+        Status solverStatus = solver.check();
         if(solver.check() == Status.SATISFIABLE){
-            //System.out.println("satisfiable");
             Model m = solver.getModel();
             LinkedList<String> new_inputs = new LinkedList<String>();
             for(MyVar v : PathTracker.inputs){
                 new_inputs.add(m.evaluate(v.z3var, true).toString());
             }
-            SymbolicExecutionLab.newSatisfiableInput(new_inputs);
+            SymbolicExecutionLab.newSatisfiableInput(new_inputs); // <---- here is when
         } else {
             //System.out.println("unsatisfiable");
         }
         // Restore the state of the solver to remove the branch constraint
         solver.pop();
+        return solverStatus;
     }
 
     // Making temporary variables, i.e., within if-conditions
